e91bd652fa0c4572400071dd250118206df1a3e9
ChitraNallasamy <91539731+ChitraNallasamy@users.noreply.github.com>
2022-05-09 12:56:36 +0530

    Update testSender.cpp

--- testSender.cpp
+++ testSender.cpp
@@ -25,3 +25,28 @@ SCENARIO("Validate JSON Data format")
         }
     }
 }
+
+SCENARIO("Validate data stream in newline")
+{
+    GIVEN("sensor data in json format")
+    {
+		FILE *endPoint = fopen("./output.txt", "w");
+		char *sensorData = "{\"Temperature\": 23.7, \"ChargingCurrent\": 5.6}";
+		char outputBuf[64];
+		char *expectedData = "{\"Temperature\": 23.7, \"ChargingCurrent\": 5.6}\n";
+        WHEN("streamData() is called with given sensor data")
+        {
+            streamData (sensorData, endPoint);
+			fclose(endPoint);
+			endPoint = fopen("./output.txt", "r");
+			fgets(outputBuf, strlen(sensorData)+2, endPoint);
+			fclose(endPoint);
+			
+			printf ("%lu:%lu\n", strlen(expectedData), strlen(outputBuf));
+            THEN("output sensor data will be generated with new line")
+            {
+                REQUIRE(strlen(outputBuf) == strlen(expectedData));
+            }
+        }
+    }
+}
